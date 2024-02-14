591db049b68ae689dfcbe51031a64777f4b055c8
Silovisk <silasaraujo1@hotmail.com>
2024-02-13 23:06:53 -0300

    finished sorteador screen

--- improving-jquery/public/assets/js/admin/sorteador/index.js
+++ improving-jquery/public/assets/js/admin/sorteador/index.js
@@ -1,35 +1,31 @@
 const PageSorteador = {
-    init: () => {
+    init: function() {
         console.log("PageSorteador init");
-        PageSorteador.sortear();
+        this.sortear();
+        this.reiniciar();
     },
 
-    sortear: () => {
-        $("#btn-sortear").on("click", function () {
-            let quantidadeDeNumeros = parseInt($("#quantidade").val(), 10);
-            let numeroDe = parseInt($("#de").val(), 10);
-            let numeroAte = parseInt($("#ate").val(), 10);
-            let resultados = [];
+    getNumber: function(id) {
+        return parseInt($(id).val(), 10);
+    },
 
-            for (let i = 0; i < quantidadeDeNumeros; i++) {
-                let randomNum =
-                    Math.floor(Math.random() * (numeroAte - numeroDe + 1)) +
-                    numeroDe;
-                resultados.push(randomNum);
-            }
-
-            $("#resultado")
-                .children()
-                .text("Números sorteados: " + resultados.join(", "));
+    sortear: function() {
+        const btnSortear = $("#btn-sortear");
+        btnSortear.off('click').on("click", () => {
+            const quantidadeDeNumeros = this.getNumber("#quantidade");
+            const numeroDe = this.getNumber("#de");
+            const numeroAte = this.getNumber("#ate");
+            const resultados = Array.from({length: quantidadeDeNumeros}, () => Math.floor(Math.random() * (numeroAte - numeroDe + 1)) + numeroDe);
+            $("#resultado").children().text("Números sorteados: " + resultados.join(", "));
         });
+        
+    },
 
-        $("#btn-reiniciar").on("click", function () {
-            $("#quantidade").val("");
-            $("#de").val("");
-            $("#ate").val("");
-            $("#resultado")
-                .children()
-                .text('Números sorteados: nenhum até agora')
+    reiniciar: function() {
+        const btnReiniciar = $("#btn-reiniciar");
+        btnReiniciar.off('click').on("click", () => {
+            ["#quantidade", "#de", "#ate"].forEach(id => $(id).val(""));
+            $("#resultado").children().text('Números sorteados: nenhum até agora');
         });
     },
 };
